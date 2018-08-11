1b5ee771ccc3362d34d09ddce1e4505c3aa9266c
Ajinath kumbhar <ajinathkumbhar@gmail.com>
2018-08-11 23:12:59 +0530

    gstreamer 1

--- c++/Pthread/gstreamer/app.cpp
+++ c++/Pthread/gstreamer/app.cpp
@@ -2,38 +2,40 @@
 #include <stdlib.h>
 #include <iostream>
 #include <gst/gst.h>
+#include <pthread.h>
+#include <unistd.h>
+
 #include "log.h"
 
+
 #define TAG "apptest"
-
-#ifndef SRC_1
 #define SRC_1 "playbin uri=file:///home/ajinath/work/00_Me/c_cpp_shell_script_python/c++/Pthread/gstreamer/stop_eng_india.mp3"
-#endif
-
-#ifndef SRC_2
 #define SRC_2 "playbin uri=file:///home/ajinath/work/00_Me/media/mp3/Tuhi_Hai-(Mr-Jatt.com).mp3"
-#endif
+
 
 
 using namespace std;
-int mArgc;
-char **mArgv = NULL;
 
-void cmdSession(const char * src) {
+void * createPipelineAndPlay(void * src) {
+	const char * src_file = (const char * ) src;
+
   GstElement *pipeline;
   GstBus *bus;
   GstMessage *msg;
 
-  /* Initialize GStreamer */
-  gst_init (&mArgc, &mArgv);
-
   /* Build the pipeline */
   // uri=file:///path/to/song.mp3
-  pipeline = gst_parse_launch (src, NULL);
+  pipeline = gst_parse_launch (src_file, NULL);
 
   /* Start playing */
-  gst_element_set_state (pipeline, GST_STATE_PLAYING);
-
+  while(1) {
+    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+    LOGD("GST_STATE_PLAYING");
+    usleep(2000000);
+    gst_element_set_state (pipeline, GST_STATE_PAUSED);
+    LOGD("GST_STATE_PAUSED");
+    usleep(2000000);
+  }
   /* Wait until error or EOS */
   bus = gst_element_get_bus (pipeline);
   msg = gst_bus_timed_pop_filtered (bus, GST_CLOCK_TIME_NONE, (GstMessageType) (GST_MESSAGE_ERROR | GST_MESSAGE_EOS));
@@ -46,13 +48,23 @@ void cmdSession(const char * src) {
   gst_object_unref (pipeline);
 }
 
+
 int main(int argc, char *argv[])
 {
-	int mArgc = argc;
-	char **mArgv = argv;
+
+	pthread_t command_proc, music_proc;
+  /* Initialize GStreamer */
+  gst_init (&argc, &argv);
+
+	//pthread_create(&command_proc, NULL, createPipelineAndPlay, (void *)SRC_1);
+  pthread_create(&music_proc, NULL, createPipelineAndPlay, (void *)SRC_2);
+  usleep(100000);
+
+	//pthread_join(command_proc, NULL);
+  pthread_join(music_proc, NULL);
+
 	LOGD("start");
-	cmdSession(SRC_1);
-	cmdSession(SRC_2);
+
 
 	return 0;
 }
